5e19b50e5b9af5152fd271b1a6199d6a09c1efa2
arajpur2 <112654333+arajpur2@users.noreply.github.com>
2023-04-28 22:24:34 -0700

    get method updated

--- src/Tokens.py
+++ src/Tokens.py
@@ -1,51 +1,44 @@
 from tokenize import tokenize, untokenize
 from io import BytesIO
 
-keywords = ["start","finish","int","bool","st","if","then","else","fi","while","begin","end","for","in","range","print","and","or","true","false","not"]
-operators = ["+", "-", "*", "/", "=", ">", "<", "!", "?", ":"]
-arithmetic_assignment = ["==","!=","<=",">="]
-separators = ["(", ")",  ","]
+KEYWORDS = ["start","finish","int","bool","st","if","then","else","fi","while","begin","end","for","in","range","print","and","or","true","false","not"]
+OPERATORS = ["+", "-", "*", "/", "=", ">", "<", "!", "?", ":"]
+ARITHMETIC_ASSIGNMENT = ["==","!=","<=",">="]
+SEPARATORS = ["(", ")",  ","]
 
 def get_tokens(file):
-    ext=file.split('.')
+    ext = file.split('.')
     print(ext)
-    if ext[1]!= "daman":
+    if ext[1] != "daman":
         print("Unsupported file extension")
         return
+    
     final_op = "["
-    process = open(file, 'r').read()
-    value = tokenize(BytesIO(process.encode('utf-8')).readline)
-    listValue = []
+
+    with open(file, 'r') as f:
+        value = tokenize(BytesIO(f.read().encode('utf-8')).readline)
 
     for tokenNum, val, _, _, _ in value:
         if len(val) == 0:
             continue
-        else:
-            if val == "\n" or val == " " or val == "\t":
-                continue
-            else:
-                    if val in keywords or val in operators or val == ";":
-                        final_op += "'"+val+"'"
-                        final_op += ", "
-                    elif val in separators:
-                        final_op += "'"
-                        final_op += val
-                        final_op += "', "
-                    elif val.startswith("'"):
-                        temp = val[1:-1]
-                        final_op += ("'" + temp + "', ")
-                    elif val.startswith('"'):
-                        temp = val[1:-1]
-                        final_op += ('"' + temp + '", ')
-                    elif val in arithmetic_assignment:
-                        final_op += "'"+val+"'"
-                        final_op += ", "
-                    elif val == ".":
-                        final_op += "'.',"
-                    elif val.isdigit():
-                        final_op += val +","
-                    elif val.isalpha():
-                        final_op += "'"+val+"',"
+        elif val == "\n" or val == " " or val == "\t":
+            continue
+        elif val in KEYWORDS or val in OPERATORS or val == ";":
+            final_op += f"'{val}', "
+        elif val in SEPARATORS:
+            final_op += f"'{val}', "
+        elif val.startswith("'") or val.startswith('"'):
+            temp = val[1:-1]
+            final_op += f"'{temp}', "
+        elif val in ARITHMETIC_ASSIGNMENT:
+            final_op += f"'{val}', "
+        elif val == ".":
+            final_op += "'.',"
+        elif val.isdigit():
+            final_op += f"{val},"
+        elif val.isalpha():
+            final_op += f"'{val}',"
+
     final_op = final_op[:-1]
     final_op += "]"
     print(final_op)
@@ -53,5 +46,5 @@ def get_tokens(file):
 
 
 if __name__ == "__main__":
-    x = input("Enter the name of the file ")
+    x = input("Enter the name of the file: ")
     get_tokens(x)
