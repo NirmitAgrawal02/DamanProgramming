5a60604b63dab5461c6de4a8600d8329066f4344
Akshat Bakliwal <abakliw1@asu.edu>
2023-04-29 16:10:10 -0700

    Changes in Tokenizer

--- daman files/Tokens2.py
+++ daman files/Tokens2.py
@@ -1,12 +1,18 @@
 from tokenize import tokenize, untokenize
 from io import BytesIO
+from pyswip import Prolog
 
-defined_terms = ["start","finish","int","bool","st","if","then","else","fi","while","begin","end","for","in","range","print","and","or","true","false","not","+", "-", "*", "/", "=", ">", "<", "!", "?", ":","==","!=","<=",">=","(", ")",  ",", ".",";"]
+prolog = Prolog()
+defined_terms = ["start", "finish", "int", "bool", "st", "if", "then", "else", "fi", "while", "begin", "end", "for", "in", "range", "print",
+                 "and", "or", "true", "false", "not", "+", "-", "*", "/", "=", ">", "<", "!", "?", ":", "==", "!=", "<=", ">=", "(", ")",  ",", ".", ";"]
+
+prolog.consult("../src/project_src.pl")
+
 
 def get_tokens(file):
-    ext=file.split('.')
+    ext = file.split('.')
     print(ext)
-    if ext[1]!= "daman":
+    if ext[1] != "daman":
         print("Unsupported file extension")
         return
     final_op = "["
@@ -21,34 +27,48 @@ def get_tokens(file):
             if val == "\n" or val == " " or val == "\t":
                 continue
             else:
-                    if val in defined_terms:
-                        final_op += "'"+val+"'"
-                        final_op += ", "
-                    elif val in separators:
-                        final_op += "'"
-                        final_op += val
-                        final_op += "', "
-                    elif val.startswith("'"):
-                        temp = val[1:-1]
-                        final_op += ("'" + temp + "', ")
-                    elif val.startswith('"'):
-                        temp = val[1:-1]
-                        final_op += ('"' + temp + '", ')
-                    elif val in arithmetic_assignment:
-                        final_op += "'"+val+"'"
-                        final_op += ", "
-                    elif val == ".":
-                        final_op += "'.',"
-                    elif val.isdigit():
-                        final_op += val +","
-                    elif val.isalpha():
-                        final_op += "'"+val+"',"
+                if val in defined_terms:
+                    final_op += "'"+val+"'"
+                    final_op += ", "
+                elif val in separators:
+                    final_op += "'"
+                    final_op += val
+                    final_op += "', "
+                elif val.startswith("'"):
+                    temp = val[1:-1]
+                    final_op += ("'" + temp + "', ")
+                elif val.startswith('"'):
+                    temp = val[1:-1]
+                    final_op += ('"' + temp + '", ')
+                elif val in arithmetic_assignment:
+                    final_op += "'"+val+"'"
+                    final_op += ", "
+                elif val == ".":
+                    final_op += "'.',"
+                elif val.isdigit():
+                    final_op += val + ","
+                elif val.isalpha():
+                    final_op += "'"+val+"',"
     final_op = final_op[:-1]
     final_op += "]"
     print(final_op)
+
     return final_op
+
+
+def evaluator(tok):
+    res = ""
+    for sol in prolog.query("program(P,"+tok+",[]."):
+        res = sol["P"]
+    if not res:
+        print("No Tree")
+        return None
+    res = res.replace("b'", "'")
+    for fin in prolog.query("program_eval("+res+",EnvOut)."):
+        pass
+    return fin
 
 
 if _name_ == "_main_":
     x = input("Enter the name of the file ")
-    get_tokens(x)
+    evaluator(get_tokens(x))
